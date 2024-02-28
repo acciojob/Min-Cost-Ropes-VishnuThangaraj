a80e2f8572487f0f29a06a0b75948f0f83cec2c9
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2024-02-28 19:44:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  arr = arr.sort(function(a,b){
-	  return a-b;
-  });
+  arr.sort(function(a,b){return a-b;});
 
 	let result = 0;
 	
@@ -15,7 +13,7 @@ function mincost(arr)
 		result += first + second;
 		arr.push(first+second);
 		arr.sort(function(a,b){return a-b;});
-	  });
+	  }
 	}
 
 	return result;
